161e199f19756a44b44ed89de39769d5fd0b41d8
bitsofbits <tim@bitsofbits.com>
2016-04-06 06:27:18 -0700

    add hacked scoring.py and notebook

--- scoring.py
+++ scoring.py
@@ -8,34 +8,38 @@ import sys
 import graph_score
 import graph_precall
 from utils import *
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+
+def augment(x, grade):
+    x = np.transpose(x)
+
+    return x
+
+    n, m = x.shape
+    m1 = m + m*(m-1)//2
+    a = np.zeros([n, grade * m1])
+    a[:,:m] = x
+    ndx = m
+    for i in range(m):
+        for j in range(i+1,m):
+            a[:,ndx] = x[:,i] * x[:,j]
+            ndx += 1
+    for i in range(2, grade+1):
+        a[:,ndx:ndx+m1] = a[:,:m1] ** i
+        ndx += m1
+    assert ndx == a.shape[-1], (ndx, a.shape, m1, grade)
+    return a
 
 def fit_score(x, windows, grade=4, lambda_val=0.0001):
-    # A pretty straight-forward implementation of regularized
-    # logistical regression without an analytical gradient...
-
-    cols = get_polynomial_cols(x, windows)
-    y = x['classification']
-    m = float(y.shape[0])
-
-    def objective(theta):
-        h = zmpolynomial(cols, *theta)
-
-        a = -y*np.log(np.where(h == 0., 1e-100, h))
-        a = np.where(y == 0., 0., a)
-        b = -(1. - y)*np.log(np.where(h == 1., 1e-100, 1. - h))
-        b = np.where(y == 1., 0., b)
-
-        J = np.sum(a + b) / m
-        Jreg = J + lambda_val * np.sum(theta**2) / (2 * m)
-        return Jreg
-  
-    return scipy.optimize.minimize(objective, [1]*(len(cols)*grade)).x
-
-    # return scipy.optimize.curve_fit(
-    #     zmpolynomial,
-    #     cols,
-    #     x['classification'],
-    #     [1]*(len(cols)*grade))[0]
+    cols = augment(get_polynomial_cols(x, windows), grade)
+    y = x['classification'].astype(int)
+    # Not sure of the exact relationship between lambda and C
+    # except that they have an inverse relationship
+#     classifier = LogisticRegression(C=1/lambda_val)
+    classifier = RandomForestClassifier(n_estimators=100)
+    classifier.fit(cols, y)
+    return classifier
 
 def optimize_window_size(xtrain, xcross, grade = 4):
     print "Fitting an optimal window size for avg/stddev columns"
@@ -47,11 +51,13 @@ def optimize_window_size(xtrain, xcross, grade = 4):
     for idx, window in enumerate(windows):
         print "%s: Fitting..." % window,
         sys.stdout.flush()
-        score_args = fit_score(xtrain, [window], grade)
+        classifier = fit_score(xtrain, [window], grade)
         print "Scoring...",
         sys.stdout.flush()
-        xtrain['score'][:] = zmpolynomial(get_polynomial_cols(xtrain, [window]), *score_args)
-        xcross['score'][:] = zmpolynomial(get_polynomial_cols(xcross, [window]), *score_args)
+        xtrain['score'][:] = classifier.predict_proba(
+            augment(get_polynomial_cols(xtrain, [window]), grade))[:,1]
+        xcross['score'][:] = classifier.predict_proba(
+            augment(get_polynomial_cols(xcross, [window]), grade))[:,1]
 
         print "Calc.err...",
         sys.stdout.flush()
@@ -86,12 +92,13 @@ def optimize_polynomial_degree(xtrain, xcross, min_window, max_grade = 10):
     for idx, grade in enumerate(grades):
         print "%s: Fitting..." % grade,
         sys.stdout.flush()
-        score_args = fit_score(xtrain, windows, grade)
+        classifier = fit_score(xtrain, windows, grade)
         print "Scoring...",
         sys.stdout.flush()
-        xtrain['score'][:] = zmpolynomial(get_polynomial_cols(xtrain, windows), *score_args)
-        xcross['score'][:] = zmpolynomial(get_polynomial_cols(xcross, windows), *score_args)
-
+        xtrain['score'][:] = classifier.predict_proba(
+            augment(get_polynomial_cols(xtrain, [min_window]), grade))[:,1]
+        xcross['score'][:] = classifier.predict_proba(
+            augment(get_polynomial_cols(xcross, [min_window]), grade))[:,1]
         print "Calc.err...",
         sys.stdout.flush()
         xtrainclassified = xtrain[xtrain["classification"] != np.Inf]
@@ -111,13 +118,14 @@ def optimize_polynomial_degree(xtrain, xcross, min_window, max_grade = 10):
 
 def train_and_score(xtrain, xtest, grade, min_window):
     windows = [min_window]
-    score_args = fit_score(xtrain, windows, grade)
-    xtest['score'][:] = zmpolynomial(get_polynomial_cols(xtest, windows), *score_args)
-    return score_args
+    classifier = fit_score(xtrain, windows, grade)
+    xtest['score'][:] = classifier.predict_proba(
+        augment(get_polynomial_cols(xtest, [min_window]), grade))[:,1]
+    return classifier
 
 def evaluate_score(xtrain, xtest, grade, min_window):
-    score_args = train_and_score(xtrain, xtest, grade, min_window)
+    classifier = train_and_score(xtrain, xtest, grade, min_window)
     graph_score.graph_score(xtest, "score")
     graph_precall.graph_precall(xtest, "score")
     print "Score window:", min_window
-    print "Score polynomial:", score_args
+#     print "Score polynomial:", score_args
